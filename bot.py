6b8d3e219ca45d6a5c7e989268e6bed54073d21a
Guest <guest@kip2.whf.local>
2024-12-18 15:06:50 -0600

    got through asking first question

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from gofish.gofish import Player, Card
 class Bot:
   def __init__(self):
     self.me = None
+    self.opponents = {}
 
   def updateStatus(self):
     status_yaml = ""
@@ -21,7 +22,21 @@ class Bot:
       script = sys.executable + " " + " ".join(sys.argv)
       self.me = Player(status["you"]["id"], script)
       for card_str in status["you"]["hand"]:
-        self.me.addCard(Card.createfromStr(card_str))
+        self.me.addToHand(Card.createFromStr(card_str))
+    for player in status["players"]:
+      if player != self.me.id:
+        self.opponents[player] = status["players"][player]
+  def processTurn(self):
+    opponent, rank = self.determinePlay()
+    self.ask(opponent, rank)
+  def determinePlay(self):
+    # Always return the very first opponent.
+    opponent = list(self.opponents.keys())[0]
+    # Always return the very first card rank.
+    rank = self.me.hand[0].rank
+    return opponent, rank
+  def ask(self, opponent, rank):
+    self.print(f"{opponent} do you have any {rank}s")
   def print(self, value):
     logging.debug("Printed: %s", value)
     print(value, flush=True)
@@ -35,6 +50,11 @@ class Bot:
       if line.strip() == "STATUS":
         logging.debug('Found STATUS command from game master')
         self.updateStatus()
+      if self.me and isinstance(self.me, Player):
+        if line.strip() == f"GO {self.me.id}":
+          logging.debug("It's my turn")
+          self.processTurn()
+        
 
 
 
